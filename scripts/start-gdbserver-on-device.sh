bf1f4fdee5133d6579137d1b54747e1594ef813a
cuijianhua <cuijianhua@jiigan.com>
2024-11-12 16:50:20 +0800

    add push only option for start-gdbesrver

--- scripts/start-gdbserver-on-device.sh
+++ scripts/start-gdbserver-on-device.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -ex
 
+push_only=0
+if [ -n "$1" ]; then
+    push_only=$1
+fi
+
 DEVICE_DIR="/data/local/tmp/cjh"
 DEVICE_IP_PORT="192.168.31.44:8848"
 TARGET_ARGS="-m '/data/local/tmp/cjh/Qwen__Qwen2.5-1.5B-Instruct-Q8_0.gguf' -p '请介绍一下成都这座城市' -n 256"
@@ -28,5 +33,7 @@ adb shell "chmod +x ${DEVICE_DIR}/gdbserver"
 adb shell "chmod +x ${DEVICE_DIR}/llama-cli"
 echo -e "\033[32mUse target remote ${DEVICE_IP_PORT} in GDB to connect to device\033[0m"
 
-# 这里使用 sh -c 来处理复杂的命令和参数
-adb exec-out "sh -c 'cd ${DEVICE_DIR}; export LD_LIBRARY_PATH=./; ./gdbserver ${DEVICE_IP_PORT} ./llama-cli ${TARGET_ARGS}'"
+if [ $push_only -eq 0 ]; then
+    # 这里使用 sh -c 来处理复杂的命令和参数
+    adb exec-out "sh -c 'cd ${DEVICE_DIR}; export LD_LIBRARY_PATH=./; ./gdbserver ${DEVICE_IP_PORT} ./llama-cli ${TARGET_ARGS}'"
+fi
