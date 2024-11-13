0188bba06fd194ce5fad4634ec9ac10460963579
cuijianhua <cuijianhua@jiigan.com>
2024-11-13 09:42:39 +0800

    optinal push .so files

--- scripts/start-gdbserver-on-device.sh
+++ scripts/start-gdbserver-on-device.sh
@@ -7,7 +7,7 @@ if [ -n "$1" ]; then
 fi
 
 DEVICE_DIR="/data/local/tmp/cjh"
-DEVICE_IP_PORT="192.168.31.44:8848"
+DEVICE_IP_PORT="10.235.112.123:8848"
 TARGET_ARGS="-m '/data/local/tmp/cjh/Qwen__Qwen2.5-1.5B-Instruct-Q8_0.gguf' -p '请介绍一下成都这座城市' -n 256"
 
 SRC_DIR=$(readlink -f "`dirname $0`")
@@ -24,10 +24,17 @@ else
 fi
 
 adb push --sync $ANDROID_NDK_HOME/prebuilt/android-arm64/gdbserver/gdbserver ${DEVICE_DIR}/
-adb push ${WORK_DIR}/build/bin/llama-cli ${DEVICE_DIR}/
-adb push ${WORK_DIR}/build/src/libllama.so ${DEVICE_DIR}/
-adb push ${WORK_DIR}/build/examples/llava/libllava_shared.so ${DEVICE_DIR}/
-adb push ${WORK_DIR}/build/ggml/src/libggml.so ${DEVICE_DIR}/
+adb push --sync ${WORK_DIR}/build/bin/llama-cli ${DEVICE_DIR}/
+
+if [ -f "${WORK_DIR}/build/src/libllama.so" ]; then
+    adb push --sync ${WORK_DIR}/build/src/libllama.so ${DEVICE_DIR}/
+fi
+if [ -f "${WORK_DIR}/build/examples/llava/libllava_shared.so" ]; then
+    adb push --sync ${WORK_DIR}/build/examples/llava/libllava_shared.so ${DEVICE_DIR}/
+fi
+if [ -f "${WORK_DIR}/build/ggml/src/libggml.so" ]; then
+    adb push --sync ${WORK_DIR}/build/ggml/src/libggml.so ${DEVICE_DIR}/
+fi
 
 adb shell "chmod +x ${DEVICE_DIR}/gdbserver"
 adb shell "chmod +x ${DEVICE_DIR}/llama-cli"
