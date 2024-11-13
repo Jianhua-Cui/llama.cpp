3cf2ab5024a70891da1a5e145f274ae2579a1d71
cuijianhua <cuijianhua@jiigan.com>
2024-11-13 09:32:03 +0800

    default build static library

--- scripts/cross-build-android.sh
+++ scripts/cross-build-android.sh
@@ -25,7 +25,8 @@ CMAKE_PARA_ANDROID="-DCMAKE_TOOLCHAIN_FILE=${ANDROID_NDK_HOME}/build/cmake/andro
     -DGGML_LLAMAFILE=OFF \
     -DANDROID_ABI=arm64-v8a \
     -DANDROID_TOOLCHAIN=clang++ \
-    -DANDROID_PLATFORM=android-28"
+    -DANDROID_PLATFORM=android-28 \
+    -DBUILD_SHARED_LIBS=OFF"
 
 if [ "${BUILD_TYPE}" == "Release" ]; then
     CMAKE_PARA=" -DCMAKE_INSTALL_PREFIX=$(dirname ${SRC_DIR})/install \
