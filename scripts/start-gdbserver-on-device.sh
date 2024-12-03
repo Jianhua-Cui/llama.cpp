d9f9cc806a30c4d86707669f9af3d3ce7af36854
cuijianhua <cuijianhua@megvii.com>
2024-12-03 22:55:06 +0800

    refine build & gdbserver scripts

--- scripts/start-gdbserver-on-device.sh
+++ scripts/start-gdbserver-on-device.sh
@@ -7,7 +7,8 @@ if [ -n "$1" ]; then
 fi
 
 DEVICE_DIR="/data/local/tmp/cjh"
-DEVICE_IP_PORT="10.235.112.162:8848"
+DEVICE_IP_PORT="192.168.31.167:8848"
+EXECUTABLE="llama-cli"
 TARGET_ARGS="-m '/data/local/tmp/cjh/Qwen__Qwen2.5-1.5B-Instruct-Q8_0.gguf' -p '请介绍一下成都这座城市' -n 256"
 
 SRC_DIR=$(readlink -f "`dirname $0`")
@@ -24,23 +25,13 @@ else
 fi
 
 adb push --sync $ANDROID_NDK_HOME/prebuilt/android-arm64/gdbserver/gdbserver ${DEVICE_DIR}/
-adb push --sync ${WORK_DIR}/build/bin/llama-cli ${DEVICE_DIR}/
-
-if [ -f "${WORK_DIR}/build/src/libllama.so" ]; then
-    adb push --sync ${WORK_DIR}/build/src/libllama.so ${DEVICE_DIR}/
-fi
-if [ -f "${WORK_DIR}/build/examples/llava/libllava_shared.so" ]; then
-    adb push --sync ${WORK_DIR}/build/examples/llava/libllava_shared.so ${DEVICE_DIR}/
-fi
-if [ -f "${WORK_DIR}/build/ggml/src/libggml.so" ]; then
-    adb push --sync ${WORK_DIR}/build/ggml/src/libggml.so ${DEVICE_DIR}/
-fi
+adb push ${WORK_DIR}/build/bin/${EXECUTABLE} ${DEVICE_DIR}/
 
 adb shell "chmod +x ${DEVICE_DIR}/gdbserver"
-adb shell "chmod +x ${DEVICE_DIR}/llama-cli"
+adb shell "chmod +x ${DEVICE_DIR}/${EXECUTABLE}"
 echo -e "\033[32mUse target remote ${DEVICE_IP_PORT} in GDB to connect to device\033[0m"
 
 if [ $push_only -eq 0 ]; then
     # 这里使用 sh -c 来处理复杂的命令和参数
-    adb exec-out "sh -c 'cd ${DEVICE_DIR}; export LD_LIBRARY_PATH=./; ./gdbserver ${DEVICE_IP_PORT} ./llama-cli ${TARGET_ARGS}'"
+    adb exec-out "sh -c 'cd ${DEVICE_DIR}; export LD_LIBRARY_PATH=./; ./gdbserver ${DEVICE_IP_PORT} ./${EXECUTABLE} ${TARGET_ARGS}'"
 fi
